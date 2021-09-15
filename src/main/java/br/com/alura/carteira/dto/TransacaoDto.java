aa4ec48133f8527799e4836436f641431a2b7dae
Henrique Lustosa <henriqlustosa@gmail.com>
2021-09-15 11:26:57 -0300

    Finalizado o exemplo do curso

--- src/main/java/br/com/alura/carteira/dto/TransacaoDto.java
+++ src/main/java/br/com/alura/carteira/dto/TransacaoDto.java
@@ -3,10 +3,7 @@ package br.com.alura.carteira.dto;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PastOrPresent;
-import javax.validation.constraints.Size;
+
 
 import br.com.alura.carteira.modelo.TipoTransacao;
 import lombok.Getter;
